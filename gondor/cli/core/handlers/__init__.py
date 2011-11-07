7ed9363a3a76070c5bd4b04fefd94968c900bc3c
Donald Stufft <donald.stufft@gmail.com>
2011-11-07 08:47:20 -0500

    add the ability to use mercurial as the vcs

--- gondor/cli/core/handlers/__init__.py
+++ gondor/cli/core/handlers/__init__.py
@@ -2,10 +2,12 @@ from cement2.core import handler
 
 from gondor.cli.core.handlers.outputs import StreamOutputHandler
 from gondor.cli.core.handlers.packagers import GitProjectPackager
+from gondor.cli.core.handlers.packagers import MercurialProjectPackager
 from gondor.cli.core.handlers.plugins import GondorPluginHandler
 
 
 def register():
     handler.register(StreamOutputHandler)
     handler.register(GitProjectPackager)
+    handler.register(MercurialProjectPackager)
     handler.register(GondorPluginHandler)
--- gondor/cli/core/handlers/packagers.py
+++ gondor/cli/core/handlers/packagers.py
@@ -1,3 +1,4 @@
+import re
 import os
 
 from gondor import utils
@@ -31,6 +32,7 @@ class GitProjectPackager(BaseProjectPackager):
             check, sha = utils.run_proc([git, "rev-parse", commit])
             
             if check != 0:
+                # @@@ Do Something for the Error Condition Here
                 print "could not map '%s' to a SHA\n" % commit
             
             if commit == "HEAD":
@@ -42,6 +44,54 @@ class GitProjectPackager(BaseProjectPackager):
             check, output = utils.run_proc(cmd, cwd=self.config.get("project", "repo_root"))
             
             if check != 0:
+                print output
+        except:
+            if tar_path and os.path.exists(tar_path):
+                os.unlink(tar_path)
+            return None, commit, sha
+        else:
+            return tar_path, commit, sha
+
+
+class MercurialProjectPackager(BaseProjectPackager):
+    class meta:
+        interface = ProjectPackager
+        label = "hg"
+    
+    def get_repo_root(self, directory):
+        return utils.find_nearest(directory, ".hg")
+    
+    def package_project(self, label, commit):
+        sha = None
+        tar_path = None
+        
+        try:
+            try:
+                hg = utils.find_command("hg")
+            except utils.BadCommand:
+                raise  # @@@ Do Something for the Error Condition Here
+            
+            branches_stdout = utils.run_proc([hg, "branches"])[1]
+            tags_stdout = utils.run_proc([hg, "tags"])[1]
+            
+            refs = {}
+            for line in branches_stdout.splitlines() + tags_stdout.splitlines():
+                m = re.search(r"([\w\d\.-]+)\s*([\d]+):([\w]+)$", line)
+                if m:
+                    refs[m.group(1)] = m.group(3)
+            
+            try:
+                sha = refs[commit]
+            except KeyError:
+                raise  # @@@ Do Something for the Error Condition Here
+            
+            tar_path = os.path.abspath(os.path.join(self.config.get("project", "repo_root"), "%s-%s.tar" % (label, sha)))
+            cmd = [hg, "archive", "-p", ".", "-t", "tar", "-r", commit, tar_path]
+            
+            check, output = utils.run_proc(cmd, cwd=self.config.get("project", "repo_root"))
+            
+            if check != 0:
+                # @@@ Do Something for the error here
                 print output
         except:
             if tar_path and os.path.exists(tar_path):
